7dcd0de07fca6b901ec720c36d21b07f109415f5
Jake Browning <jake.browning@emu-analytics.com>
2019-02-05 11:29:56 +0000

    Add comments

--- ishmael.js
+++ ishmael.js
@@ -7,13 +7,21 @@ class Ishmael {
     this.options = {
       delay: 1000,
       fps: 40,
-      charset: 'qwertyuiopasdfghjklzxcvbnm_____________________',
+      charset: 'abcdefghijklmnopqrstuvwxyz_______________',
       repeat: true,
       fromEmpty: true,
-      flicker: true
+      flicker: true,
+      direction: 'random'
     }
   }
 
+  /**
+   * Perform a Ishmael effect on an array of sentences.
+   *
+   * @param sentences
+   * @param options
+   * @param firstIteration
+   */
   transition(sentences, options = this.options, firstIteration = true) {
     const funcs = [];
     const baseIndex = (this.options.fromEmpty || !firstIteration) ? 0 : 1;
@@ -41,6 +49,16 @@ class Ishmael {
   }
 
 
+  /**
+   * Given two sentences, Ishmael from one to the other recursively.
+   *
+   * @param str1
+   * @param str2
+   * @param flippedIndexes
+   * @param iterations
+   * @returns {*}
+   * @private
+   */
   _transitionPair(str1, str2, flippedIndexes = [], iterations = 0) {
     this.node.innerHTML = this._style(str1);
 
@@ -67,17 +85,18 @@ class Ishmael {
     }
     else {
       const noiseIndex = Math.floor(Math.random() * adjustedStr1.length);
-      if ((flippedIndexes.includes(noiseIndex) && Math.floor(Math.random() * iterations) == 0 && this.options.flicker)
-          || !flippedIndexes.includes(noiseIndex)) {
+
+      if (this._addNoise(flippedIndexes, noiseIndex, iterations)) {
         const randomChar = Ishmael._randomChar(charset);
         adjustedStr1 = `${Ishmael._setCharAt(adjustedStr1, noiseIndex, randomChar)}`;
         this.noiseIndexes.push(noiseIndex);
+
         if (flippedIndexes.includes(noiseIndex)) {
           flippedIndexes = flippedIndexes.filter(i => i !== noiseIndex);
         }
       }
 
-      const flipIndex = Ishmael._getFlipIndex(flippedIndexes, adjustedStr1.length);
+      const flipIndex = this._getFlipIndex(flippedIndexes, adjustedStr1.length);
       flippedIndexes.push(flipIndex);
 
       adjustedStr1 = Ishmael._setCharAt(adjustedStr1, flipIndex, adjustedStr2.charAt(flipIndex));
@@ -94,6 +113,14 @@ class Ishmael {
     });
   }
 
+  /**
+   * Add relevant HTML tags to string so that different types of
+   * characters can be styled in the CSS.
+   * 
+   * @param str
+   * @returns {*}
+   * @private
+   */
   _style(str) {
     if (this.noiseIndexes.length === 0) return str;
     this.noiseIndexes = this.noiseIndexes.sort((a, b) => a - b);
@@ -108,22 +135,30 @@ class Ishmael {
     return newStrs.join('');
   }
 
-  static _getFlipIndex(flippedIndexes, len) {
-    const zeroToLen = Array.apply(null, {length: len}).map(Number.call, Number);
-    const unflippedIndexes = Ishmael._diff(flippedIndexes, zeroToLen);
-    const flipIndex = unflippedIndexes[Math.floor(Math.random() * unflippedIndexes.length)];
-    return flipIndex;
+  /**
+   * Determine whether to add a noise character to the string, where the probability
+   * of adding noise tends to 0 as iterations increase.
+   * 
+   * @param flippedIndexes
+   * @param noiseIndex
+   * @param iterations
+   * @returns {boolean}
+   * @private
+   */
+  _addNoise(flippedIndexes, noiseIndex, iterations) {
+    return (flippedIndexes.includes(noiseIndex) &&
+        Math.floor(Math.random() * iterations) == 0 && this.options.flicker)
+        || !flippedIndexes.includes(noiseIndex);
   }
 
-  static _diff(a, b) {
-    return b.filter((i) => a.indexOf(i) < 0);
-  };
-
-  static _setCharAt(str, index, chr) {
-    if (index > str.length - 1 ) return str;
-    return str.substr(0, index) + chr + str.substr(index + 1);
-  }
-
+  /**
+   * Add a random character to the start or end of a string.
+   * 
+   * @param str
+   * @param charset
+   * @returns {string}
+   * @private
+   */
   _padOne(str, charset) {
     const prefix = (Math.round(Math.random()) === 1) ? true : false;
     const char = Ishmael._randomChar(charset);
@@ -138,10 +173,60 @@ class Ishmael {
     return (prefix) ? `${char}${str}` : `${str}${char}`;
   }
 
+  /**
+   * Remove a ranom character from the start or end of a string.
+   * 
+   * @param str
+   * @returns {Array.<T>|string|Blob|ArrayBuffer}
+   * @private
+   */
   static _removeOne(str) {
     const prefix = (Math.round(Math.random()) === 1) ? true : false;
 
     return (prefix) ? str.slice(1, -1) : str.slice(0, -1);
+  }
+
+  /**
+   * Choose which index to flip, by randomly choosing
+   * an index which hasn't been flipped yet. A flipped
+   * index is one where the letter at that index is correct for
+   * the sentence we're currently Ishmaeling to.
+   * 
+   * @param flippedIndexes
+   * @param len
+   * @returns {*}
+   * @private
+   */
+  _getFlipIndex(flippedIndexes, len) {
+    const zeroToLen = Array.apply(null, {length: len}).map(Number.call, Number);
+    const unflippedIndexes = Ishmael._diff(flippedIndexes, zeroToLen);
+
+    let flipIndex;
+    switch (this.options.direction) {
+      case 'random':
+        flipIndex = unflippedIndexes[Math.floor(Math.random() * unflippedIndexes.length)];
+        break;
+      case 'lr':
+        flipIndex = unflippedIndexes[0];
+        break;
+      case 'rl':
+        flipIndex = unflippedIndexes[unflippedIndexes.length - 1];
+        break;
+      default:
+        throw new Error('options.direction must be one of "random", "lr" or "rl".');
+        break;
+    }
+
+    return flipIndex;
+  }
+
+  static _diff(a, b) {
+    return b.filter((i) => a.indexOf(i) < 0);
+  };
+
+  static _setCharAt(str, index, chr) {
+    if (index > str.length - 1 ) return str;
+    return str.substr(0, index) + chr + str.substr(index + 1);
   }
 
   static _randomChar(charset) {
